69c45377efca9ea869e5a3ce66cb780c79771808
Virginie Helmreich <122171893+virginiacodes@users.noreply.github.com>
2024-08-16 14:46:18 +0100

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <>
+      <h1 className="about__heading page-heading">About</h1>
+      <p className="about__intro text-paragraph">
+        As a musician, I know the struggle of finding decent gigs, especially in
+        London. Small independant artists are taken advantage of and lazy
+        promoters are filling their pockets. Here we don't promote pay-for-play
+        and will always put the interest of the artists first. Let's build a
+        fair system together!
+      </p>
+    </>
+  );
+}
