876db1086346210ca31b65d3e6bfec130faf23ea
Virginie Helmreich <122171893+virginiacodes@users.noreply.github.com>
2024-08-16 14:49:42 +0100

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,36 @@ import { useState } from "react";
 import EventNav from "./EventNav";
 import EventList from "./EventList";
 import Header from "./Header";
+import About from "./About";
 
 export default function App() {
   const [isSearchOpen, setisSearchOpen] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState("about");
 
   function handleSearchOpen() {
     setisSearchOpen(!isSearchOpen);
   }
 
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
+
   return (
     <div className="App">
-      <Header isSearchOpen={isSearchOpen} onOpenSearch={handleSearchOpen} />
+      <Header
+        isSearchOpen={isSearchOpen}
+        onOpenSearch={handleSearchOpen}
+        onChangePage={handlePageChange}
+      />
       <main>
-        <EventNav />
-        <EventList />
+        {currentPage === "home" && (
+          <>
+            <EventNav />
+            <EventList />
+          </>
+        )}
+        {currentPage === "about" && <About />}
       </main>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import Navbar from "./Navbar";
 import Search from "./Search";
 
-export default function Header({ onOpenSearch, isSearchOpen }) {
+export default function Header({ onOpenSearch, isSearchOpen, onChangePage }) {
   return (
     <header className="header">
-      <h1 className="header__logo">Fair Gigs</h1>
+      <h1 className="header__logo" onClick={() => onChangePage("home")}>
+        Fair Gigs
+      </h1>
 
-      <Navbar onOpenSearch={onOpenSearch} isSearchOpen={isSearchOpen} />
+      <Navbar
+        onOpenSearch={onOpenSearch}
+        isSearchOpen={isSearchOpen}
+        onChangePage={onChangePage}
+      />
 
       <Search onOpenSearch={onOpenSearch} isSearchOpen={isSearchOpen} />
     </header>
